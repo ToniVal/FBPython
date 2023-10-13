d720c1d8bfe14e635592419a98740d51c91bae93
ToniVal <toni.valimaki@vaisto.io>
2023-10-13 15:23:25 +0300

    added OR and NOT

--- boolean operators/boolOp.py
+++ boolean operators/boolOp.py
@@ -1,6 +1,6 @@
 class boolOperators(): # Lets have different boolean operators listed here.
 
-    def bAnd(self, in1, in2): # Basic AND operation with 2 inputs, output and inverse output. 
+    def bAnd(self, in1, in2): # Basic AND operation with 2 inputs and 1 output. 
         self.in1 = in1
         self.in2 = in2
 
@@ -8,5 +8,26 @@ class boolOperators(): # Lets have different boolean operators listed here.
             out = True
         else:
             out = False
+        
+        return out
+    
+    def bOr(self, in1, in2): # Basic OR operation with 2 inputs and 1 output.
+        self.in1 = in1
+        self.in2 = in2
+
+        if self.in1 == True and self.in2 == True:
+            out = True
+        else:
+            out = False
+        
+        return out
+    
+    def bNot(self, in1): # Basic NOT operation with 1 input and 1 output.
+        self.in1 = in1
+
+        if self.in1 == True:
+            out = False
+        else:
+            out = True
         
         return out
