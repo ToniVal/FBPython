6aaa1e2445033c007b40def513d1d7f36d421304
ToniVal <toni.valimaki@vaisto.io>
2023-10-13 15:17:17 +0300

    updated AND operator

--- boolean operators/boolOp.py
+++ boolean operators/boolOp.py
@@ -1,16 +1,12 @@
 class boolOperators(): # Lets have different boolean operators listed here.
 
-    def bAnd(self, in1, in2, out, iout): # Basic AND operation with 2 inputs, output and inverse output. 
+    def bAnd(self, in1, in2): # Basic AND operation with 2 inputs, output and inverse output. 
         self.in1 = in1
         self.in2 = in2
-        self.out = out
-        self.iout = iout
 
         if self.in1 == True and self.in2 == True:
-            self.out = True
-            self.iout = False
+            out = True
         else:
-            self.out = False
-            self.iout = True
+            out = False
         
-        return self.out, self.iout
+        return out
