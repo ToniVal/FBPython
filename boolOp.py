e7be6105f618b2f63e315cd7be9666b4bc4d4be6
ToniVal <toni.valimaki@vaisto.io>
2023-10-13 15:28:39 +0300

    #

--- boolOp.py
+++ boolOp.py
@@ -1,31 +1,26 @@
 class boolOperators(): # Lets have different boolean operators listed here.
 
-    def bAnd(self, in1, in2): # Basic AND operation with 2 inputs and 1 output. 
-        self.in1 = in1
-        self.in2 = in2
+    def bAnd(in1, in2): # Basic AND operation with 2 inputs and 1 output. 
 
-        if self.in1 == True and self.in2 == True:
+        if in1 == True and in2 == True:
             out = True
         else:
             out = False
         
         return out
     
-    def bOr(self, in1, in2): # Basic OR operation with 2 inputs and 1 output.
-        self.in1 = in1
-        self.in2 = in2
+    def bOr(in1, in2): # Basic OR operation with 2 inputs and 1 output.
 
-        if self.in1 == True and self.in2 == True:
+        if in1 == True and in2 == True:
             out = True
         else:
             out = False
         
         return out
     
-    def bNot(self, in1): # Basic NOT operation with 1 input and 1 output.
-        self.in1 = in1
+    def bNot(in1): # Basic NOT operation with 1 input and 1 output.
 
-        if self.in1 == True:
+        if in1 == True:
             out = False
         else:
             out = True
