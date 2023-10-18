ef04128630abf3c5eaead4b200a74ef3e6127002
ToniVal <toni.valimaki@vaisto.io>
2023-10-18 11:27:08 +0300

    added functions to arithmetic

--- arithmeticOp.py
+++ arithmeticOp.py
@@ -1,14 +1,39 @@
 class arithmeticOperators(): # Basic arithmetic functions, probably the best way to operate with numbers is to convert all to float and carry the type through the operation.
 
-    def numInOut(opType, in1, numOutType): # Number converter and type saver.
+    def numInOut(opType, in1, numOutType): # Number converter and type saver. incomp.
         if opType == "input":
             numInType = type(in1)
             out = float(in1)
 
         if opType == "output":
-            if numOutType == "int":
+            if numOutType == "<class 'int'>":
                 out = int(in1)
-            if numOutType == "float":
+            if numOutType == "<class 'float'>":
                 out = float(in1)
 
         return out, numInType
+
+    def numAdd(in1, in2):
+
+        out = in1 + in2
+
+        return out
+    
+    def numSub(in1, in2):
+
+        out = in1 - in2
+
+        return out
+    
+    def numMult(in1, in2):
+
+        out = in1 * in2
+
+        return out
+    
+    def numDivd(in1, in2):
+
+        out = in1/in2
+
+        return out
+    
