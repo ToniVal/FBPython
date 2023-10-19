02b6281888b5ae5976009edebd74efbeffb2808b
ToniVal <toni.valimaki@vaisto.io>
2023-10-19 11:09:05 +0300

    updated arithmetic inout to simple

--- arithmeticOp.py
+++ arithmeticOp.py
@@ -1,17 +1,21 @@
 class arithmeticOperators(): # Basic arithmetic functions, probably the best way to operate with numbers is to convert all to float and carry the type through the operation.
 
-    def numInOut(opType, in1, numOutType): # Number converter and type saver. incomp.
-        if opType == "input":
-            numInType = type(in1)
-            out = float(in1)
+    def numInOut(in1): # Number converter and type saver. incomp. //opType, in1, numOutType
+        #if opType == "input":
+        #    numInType = type(in1)
+        #    out = float(in1)
 
-        if opType == "output":
-            if numOutType == "<class 'int'>":
-                out = int(in1)
-            if numOutType == "<class 'float'>":
-                out = float(in1)
+        #if opType == "output":
+        #    if numOutType == "<class 'int'>":
+        #        out = int(in1)
+        #    if numOutType == "<class 'float'>":
+        #        out = float(in1)
 
-        return out, numInType
+        # ^^Do i need those^^
+
+        out = in1
+
+        return out, #numInType
 
     def numAdd(in1, in2):
 
